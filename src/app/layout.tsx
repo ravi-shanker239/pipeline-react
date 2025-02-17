beec894e64fa0c7c99531629203e1df4a3477623
ravi-shanker239 <135230864+ravi-shanker239@users.noreply.github.com>
2025-02-17 22:12:56 +0500

    Added pre-push hook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSanss.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
